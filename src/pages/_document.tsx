8ff0952557bf4b3e59269bdb06d29a1542248b77
Leonardo Menezes <leonhard.menezes@gmail.com>
2021-05-26 15:30:27 -0300

    feat: Estrutura da aplicação - Estilos globais do app

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,16 +4,16 @@ export default class MyDocument extends Document{
   render() {
     return (
       <Html>
-      <Head>
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet" />
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet" />
 
-        <title>ig.news</title>
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
+          <title>ig.news</title>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
       </Html>
     );
   }
