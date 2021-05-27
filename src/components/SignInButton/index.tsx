f6d0a499a7a42a472df66867f47b62e731fa243a
Leonardo Menezes <leonhard.menezes@gmail.com>
2021-05-27 16:42:09 -0300

    feat(auth): API Routes no Next.js - API routes no Next.js - Estratégias de autenticação - Parametrização nas rotas - Autenticação com Next Auth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,22 +1,27 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/client'
 
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-  const userLoggedIn = true;
+  const [session] = useSession();
   
-  return userLoggedIn ? (
+  return session ? (
     <button
+      type="button"
       className={styles.signInButton}
+      onClick={() => signOut()}
     >
       <FaGithub color="#04d361"/>
-      Leonardo Menezes
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon}/>
     </button>
   ) : (
     <button
+      type="button"
       className={styles.signInButton}
+      onClick={() => signIn('github')}
     >
       <FaGithub color="#eba417"/>
       Sign in with Github
